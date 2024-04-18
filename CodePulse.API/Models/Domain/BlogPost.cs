a7876d2b55030e60bac7ce3992f155f6e044582c
Ahmed Qureshi <=>
2024-04-18 20:58:24 +0500

    Add project files.

---
+++ CodePulse.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿namespace CodePulse.API.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortContent { get; set; }
+        public string Content { get; set; }
+        public string FeatureImage { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublisherDate { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+
+    }
+}
